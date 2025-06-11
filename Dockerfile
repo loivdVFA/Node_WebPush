6b69f49f2f42cfef6669cd5547a8d1df6780e395
loivdVFA <loivd001@gmail.com>
2025-06-11 07:58:26 +0700

    add: ably

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:22.14.0-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+
+
+RUN npm install --production
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["node", "main.js"]
--- main.js
+++ main.js
@@ -1,53 +1,101 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import Ably from "ably";
 import webpush from 'web-push';
 import cors from 'cors';
 
 const app = express();
 app.use(cors({
-    origin: "*"
+  origin: "*"
 }))
 app.use(bodyParser.json());
 
-const publicVapidKey = 'BGDVQhQc-2zf5P0zFJOsQQyV2AdAsZ2e2OPdv9fpkMJiJJQEBzLNtgknJltW-_V3VPEycknwc5jIK-8jYuH8gBM';
-const privateVapidKey = 'VCDUx4x5ZVSBsB4tXneSga4O3R5BOW6iXbDS2GcBSK0';
+let dataDevice = null;
+const ably = new Ably.Realtime('CtgqHw.hG694A:KrC9gf0LMS7xkfnQtYUDmXhIyPESDjTUAUeoogMWYRw');
 
-webpush.setVapidDetails(
-  'mailto:your-email@example.com',
-  publicVapidKey,
-  privateVapidKey
-);
+app.post('/get-device', (req, res) => {
+  dataDevice = req.body.push.recipient;
+  res.json(dataDevice);
+})
 
-// Chỉ 1 user → lưu subscription tạm
-let currentSubscription = null;
-
-// Route lưu subscription
-app.post('/subscribe', (req, res) => {
-  currentSubscription = req.body.subscription;
-  console.log('Saved subscription:', currentSubscription);
-
-  res.status(201).json({});
-});
-
-// Route gửi push
-app.post('/send', async (req, res) => {
-  if (!currentSubscription) {
-    return res.status(400).json({ error: 'No subscription saved' });
+app.post('/send-notification', async (req, res) => {
+  if (!dataDevice) {
+    return res.status(400).json({ error: 'No device registered' });
   }
-  console.log(req.body);
-  
   const { title, body } = req.body;
-  const payload = JSON.stringify({ title, body });
+  console.log('deviceId:', dataDevice);
+  var recipient = {
+    deviceId: dataDevice
+  };
 
-  try {
-    await webpush.sendNotification(currentSubscription, payload);
-    console.log('Push sent!');
-    res.json({ success: true });
-  } catch (err) {
-    console.error('Push error:', err);
-    res.status(500).json({ error: 'Push failed' });
+  var data = {
+    notification: {
+      title: "Hello from Ably!",
+      body: "Example push notification from Ably.",
+      ttl: 3600
+    }
   }
+
+  ably.push.admin.publish(dataDevice, data);
+
+  // try {
+  // await ably.push.admin.publish(
+  //   { deviceId: dataDevice } ,
+  //  {
+  //     title: title || 'Test Notification',
+  //     body: body || 'This is a test notification',
+  //     ttl: 3600
+  //     // icon: 'https://your-domain.com/text.png',
+  //     // data: { custom: 'data' }
+  //   }
+  // );
+  res.json({ success: true });
+
+  // } catch (err) {
+  //   console.error('Ably push error:', err);
+  //   res.status(500).json({ error: 'Push failed' });
+  // }
 });
+
+// const publicVapidKey = 'BGDVQhQc-2zf5P0zFJOsQQyV2AdAsZ2e2OPdv9fpkMJiJJQEBzLNtgknJltW-_V3VPEycknwc5jIK-8jYuH8gBM';
+// const privateVapidKey = 'VCDUx4x5ZVSBsB4tXneSga4O3R5BOW6iXbDS2GcBSK0';
+
+// webpush.setVapidDetails(
+//   'mailto:your-email@example.com',
+//   publicVapidKey,
+//   privateVapidKey
+// );
+
+// // Chỉ 1 user → lưu subscription tạm
+// let currentSubscription = null;
+
+// // Route lưu subscription
+// app.post('/subscribe', (req, res) => {
+//   currentSubscription = req.body.subscription;
+//   console.log('Saved subscription:', currentSubscription);
+
+//   res.status(201).json({});
+// });
+
+// // Route gửi push
+// app.post('/send', async (req, res) => {
+//   if (!currentSubscription) {
+//     return res.status(400).json({ error: 'No subscription saved' });
+//   }
+//   console.log(req.body);
+
+//   const { title, body } = req.body;
+//   const payload = JSON.stringify({ title, body });
+
+//   try {
+//     await webpush.sendNotification(currentSubscription, payload);
+//     console.log('Push sent!');
+//     res.json({ success: true });
+//   } catch (err) {
+//     console.error('Push error:', err);
+//     res.status(500).json({ error: 'Push failed' });
+//   }
+// });
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
